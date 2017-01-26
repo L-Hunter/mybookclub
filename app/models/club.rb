fbc01c4010faf12b7f4143baa48faed6ccbd045f
TheButlerDidIt <labutler@gmail.com>
2017-01-26 17:35:32 -0500

    adds functionality and display for clubs

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,6 +1,6 @@
 class Club < ApplicationRecord
 	
-	has_and_belongs_to_many :meetings
+	has_many :meetings
 
 	has_attached_file :avatar
 	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
