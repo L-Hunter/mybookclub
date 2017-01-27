f5cb05534233acb49d596544b0487f72262493f9
TheButlerDidIt <labutler@gmail.com>
2017-01-27 13:09:19 -0500

    creates meetings and associated functionality

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -3,20 +3,41 @@ class MeetingsController < ApplicationController
   end
 
   def show
+    @meeting = Meeting.find(params[:id])
   end
 
   def new
   end
 
   def create
+    @meeting = Meeting.create(meeting_params)
+    if @meeting.save
+      redirect_to @meeting
+    else
+      render new_meeting_path
+    end
   end
 
   def edit
+    @meeting = Meeting.find(params[:id])
   end
 
   def update
+    @meeting = Meeting.find(params[:id])
+    @meeting.update(meeting_params)
+    redirect_to meeting_path
   end
 
   def destroy
+    @meeting = Meeting.find(params[:id])
+    @meeting.destroy
+    redirect_to club_path(@meeting.club_id)
+  end
+
+  private
+
+  def meeting_params
+    params.require(:meeting).permit(:date, :time, :host, :street, :city, :state, :zip, :comment, :club_id)
+
   end
 end
