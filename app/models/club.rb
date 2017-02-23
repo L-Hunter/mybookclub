2612e81f4a9f3aada0224195ccf66fcf183a052e
TheButlerDidIt <labutler@gmail.com>
2017-02-23 14:42:24 -0500

    setting up aws s3

--- app/models/club.rb
+++ app/models/club.rb
@@ -2,8 +2,16 @@ class Club < ApplicationRecord
 	
 	has_many :meetings
 
-	has_attached_file :avatar
+	has_attached_file :avatar, :storage => :s3, :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+
+ 	def s3_credentials
+	    {:bucket => ENV['S3_BUCKET'],
+	    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+	    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']}
+	end
+
 	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
 
 	validates :club_name, presence: true
 	validates_inclusion_of :is_public, :in => [true, false]
