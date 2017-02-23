c7af5e7458ccbe2d164ef50a42df8c7e0745c453
TheButlerDidIt <labutler@gmail.com>
2017-02-23 14:49:53 -0500

    aws

--- app/models/club.rb
+++ app/models/club.rb
@@ -5,7 +5,7 @@ class Club < ApplicationRecord
 	has_attached_file :avatar, :storage => :s3, :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
 
  	def s3_credentials
-	    {:bucket => ENV['S3_BUCKET'],
+	    {:bucket => ENV['S3_BUCKET_NAME'],
 	    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
 	    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']}
 	end
